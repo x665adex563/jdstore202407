d5b3d9b67a5fddf814c1cf287d44ad3d33080c0a
Knight~ <x665adex563@gmail.com>
2024-06-29 12:38:10 +0800

    完成購物車其他核心功能1

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,7 @@
 class CartsController < ApplicationController
+  def clean
+    current_cart.clean!
+    flash[:wargning] = "已清空購物車"
+    redirect_to carts_path
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,8 +10,11 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
 
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入購物車"
-    redirect_to :back
+
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已成功將 #{@product.title}"
+      redirect_to :back
+    end
   end
 end
